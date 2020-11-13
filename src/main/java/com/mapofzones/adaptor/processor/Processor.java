d826044409a4f324a89da04d0403d9aa87057414
Raumo0 <dharapko@gmail.com>
2020-11-12 19:51:48 -0500

    Base scheduler template

---
+++ src/main/java/com/mapofzones/adaptor/processor/Processor.java
@@ -0,0 +1,11 @@
+package com.mapofzones.adaptor.processor;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Processor {
+
+    public void doScript() {
+        System.out.println("Hello world!");
+    }
+}
