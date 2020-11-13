1552021fc6b8b7b0ebd457b385ae7ed29eed855a
Raumo0 <dharapko@gmail.com>
2020-11-14 02:05:17 +0300

    Added correct timeframes

---
+++ src/main/java/com/mapofzones/adaptor/constants/TimeframeConstants.java
@@ -0,0 +1,13 @@
+package com.mapofzones.adaptor.constants;
+
+public class TimeframeConstants {
+    public static final int DAY = 24;
+    public static final int DAY_STEP = 4;
+    public static final int WEEK = 168;
+    public static final int WEEK_STEP = 28;
+    public static final int MONTH = 720;
+    public static final int MONTH_STEP = 120;
+
+    private TimeframeConstants() {
+    }
+}
--- src/main/java/com/mapofzones/adaptor/processor/Processor.java
+++ src/main/java/com/mapofzones/adaptor/processor/Processor.java
@@ -1,5 +1,6 @@
 package com.mapofzones.adaptor.processor;
 
+import com.mapofzones.adaptor.constants.TimeframeConstants;
 import com.mapofzones.adaptor.data.entities.Header;
 import com.mapofzones.adaptor.data.repository.HeaderRepository;
 import org.springframework.stereotype.Service;
@@ -13,10 +14,16 @@ public class Processor {
     }
 
     public void doScript() {
-        System.out.println("Hello world!");
-        Header header = headerRepository.getHeaderByTimeframe(10000, 1000);
-        System.out.println(header);
+        System.out.println("Starting...");
+        Header header = headerRepository.getHeaderByTimeframe(TimeframeConstants.DAY, TimeframeConstants.DAY_STEP);
         headerRepository.save(header);
+        System.out.println("header TF " + TimeframeConstants.DAY);
+        header = headerRepository.getHeaderByTimeframe(TimeframeConstants.WEEK, TimeframeConstants.WEEK_STEP);
+        headerRepository.save(header);
+        System.out.println("header TF " + TimeframeConstants.WEEK);
+        header = headerRepository.getHeaderByTimeframe(TimeframeConstants.MONTH, TimeframeConstants.MONTH_STEP);
+        headerRepository.save(header);
+        System.out.println("header TF " + TimeframeConstants.MONTH + "\nFinished!");
         System.out.println("---------------");
     }
 }
