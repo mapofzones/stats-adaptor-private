3e3f9485da2b8d6aece04a6ebb259ef0143dd6a9
Raumo0 <dharapko@gmail.com>
2022-02-11 11:47:50 +0100

    Changed steps for charts

--- src/main/java/com/mapofzones/adaptor/constants/TimeframeConstants.java
+++ src/main/java/com/mapofzones/adaptor/constants/TimeframeConstants.java
@@ -2,9 +2,9 @@ package com.mapofzones.adaptor.constants;
 
 public interface TimeframeConstants {
     int DAY = 24;
-    int DAY_STEP = 4;
+    int DAY_STEP = 2;
     int WEEK = 168;
-    int WEEK_STEP = 28;
+    int WEEK_STEP = 14;
     int MONTH = 720;
-    int MONTH_STEP = 120;
+    int MONTH_STEP = 60;
 }
